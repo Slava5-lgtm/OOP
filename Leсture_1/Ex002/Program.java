4db76e0b0aaddc4760018619161d2fea9e7fb202
Vaceslav <vaceslavsergunin@gmail.com>
2023-02-12 01:27:27 +0600

    +

---
+++ Leсture_1/Ex002/Program.java
@@ -0,0 +1,27 @@
+package Leсture_1.Ex002;
+
+public class Program {
+//     static double distance(int x1, int y1, int x2, int y2) {
+//         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // было четыри аргумнгта
+//     }
+
+//     static double distance(Point2D a, Point2D b) {
+//         return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2)); // Теперь есть новый метод принимающий две
+//                                                                            // точки
+// }
+//     public static void main(String[] args) {
+//         Point2D a = new Point2D(); // Есть зкземпляр класса точка (а). Указываем значение одной точки
+//         a.x = 0;
+//         a.y = 2;
+//         System.out.println(a.toString());
+//         // Второй точки
+//         Point2D b = new Point2D(); // Есть зкземпляр класса точка (b).
+//         b.x = 0;
+//         b.y = 10;
+//         System.out.println(b.toString());
+//         System.out.println(distance(a, b)); // Вычеслчем значение используя метод distance (8 строка) и передаем имено
+//                                             // две точки
+
+//     }
+    
+}
